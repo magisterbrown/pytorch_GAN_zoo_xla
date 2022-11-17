ee7696f565f5f724a116d66c7e7fd0761d8c6e9c
Artur <magisterbrownie@gmail.com>
2022-11-17 14:33:35 +0000

    Starting ds

--- models/trainer/pgan_xla_trainer.py
+++ models/trainer/pgan_xla_trainer.py
@@ -3,6 +3,8 @@ from ..pgan_xla import XLAProgressiveGAN
 from .standard_configurations.pgan_config import _C
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.xla_multiprocessing as xmp
+import webdataset as wds
+import torch
 
 class PganXlaTrainer(GANTrainer):
 
@@ -50,18 +52,33 @@ class PganXlaTrainer(GANTrainer):
 
     def train(self):
         n_scales = len(self.modelConfig.depthScales)
+        flags = {
+                'seed': 420
+                }
         print(n_scales)
         print('XLA train')
-        xmp.spawn(self.mpf, args=(), nprocs=8, start_method='fork')
+        xmp.spawn(self.mpf, args=(flags), nprocs=8, start_method='fork')
 
-    def mpf(self, index):
+    def mpf(self, index, flags):
+        print(flags)
+        torch.manual_seed(420)
+        xm.rendezvous('init')
         self.model.meme+=1
         device = xm.xla_device()  
         self.model.updateSolverDeviceTpu(device)
-        print(xm.get_ordinal())
         n_scales = len(self.modelConfig.depthScales)
 
-        xm.rendezvous('init')
+        if xm.is_master_ordinal():
+            print(list(range(self.startScale, n_scales)))
+        for scale in range(self.startScale, n_scales):
+            dbLoader = self.getDBLoader(scale)
+
+    def getDBLoader(self, scale):
+        size = pow(2,scale+1)
+        shard = f'gs://monet-cool-gan/shards_monet/monet_shard_{xm.get_ordinal()}.tar'
+
+        ds = wds.WebDataset(shard).decode().map(proc.proc)
+        loader = torch.utils.data.DataLoader(ds, batch_size=4, drop_last=True)
 
 
 
